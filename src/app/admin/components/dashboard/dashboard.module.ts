12abe9bdc98f099ea630853508283b7aab057544
Deniz Seyrekbasan <dseyrekbasan@papatia.co>
2024-04-01 15:02:47 +0300

    init project

---
+++ src/app/admin/components/dashboard/dashboard.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import path from 'path';
+import { DashboardComponent } from './dashboard.component';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([{ path: "", component: DashboardComponent }]),
+  ],
+})
+export class DashboardModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { LayoutComponent } from './admin/layout/layout.component';
+import { SidebarComponent } from './admin/layout/components/sidebar/sidebar.component';
+import { HeaderComponent } from './admin/layout/components/header/header.component';
+import { FooterComponent } from './admin/layout/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LayoutComponent, RouterModule, SidebarComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ECommerceClient';
